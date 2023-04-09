7737a9aaaaf8b4e798505e3c4f5cdf69027226f7
Dominik <dominikaliashchuk@gmail.com>
2023-04-09 15:07:53 +0200

    updated the img path

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Background() {
     return (
     <div className='background'>
-        <img className="bottomLeftImg" src='/src/assets/blob1.svg' />
-        <img className="topRightImg" src='/src/assets/blob2.svg' />
+        <img className="bottomLeftImg" src='public/images/blob1.svg' />
+        <img className="topRightImg" src='public/images/blob2.svg' />
     </div>
     )
 }
